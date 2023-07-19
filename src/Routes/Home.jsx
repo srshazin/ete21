ea6aa466a49b6c171e3039276fc92555012c7c45
Shazin <shazin.bd@gmail.com>
2023-07-19 21:26:38 +0600

    Changes to be committed: 	modified:   src/Routes/Home.jsx

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import Feed from "../Components/Children/Feed";
 import Footer from "../Components/Children/Footer";
 import Header from "../Components/Children/Header";
@@ -7,6 +8,9 @@ import TechnicalStacks from "../Components/Widgets/TechnicalStack";
 const Home = () => {
   return (
     <div className="Home">
+      <Helmet>
+        <title>ETE-21 Family</title>
+      </Helmet>
       <Header />
       <Feed />
       <TechnicalStacks />
